0bf1fc4fef87110d8093f1b386da7cbad29a88f8
Sergii <sergiigrytsaienko@gmail.com>
2017-07-24 22:02:36 +0000

    updated

--- script.js
+++ script.js
@@ -1,50 +1,77 @@
-var board = [
-    '', '', '',
-    '', '', '',
-    '', '', ''
-];
+$(document).ready(function() {
 
-function Print(board) {
-    for (var i = 0; i < 9; i++) {
-        $('#' + i).text(board[i]);
-    }
+  var board = [
+      '', '', '',
+      '', '', '',
+      '', '', ''
+    ];
+
+  function Print(board) {
+      for (var i = 0; i < 9; i++) {
+          $('#' + i).text(board[i]);
+      }
+  }
+// user's turn
+function IsPlayerTurn () {
+  return Math.floor(Math.random()*2+1);
 }
 
-function IsPlayerFirst() {
-    return (Math.random() >= 0.5);
-}
+// X or O
 
-function Log(val) {
+$('.inbox').click(function () {
+        var index = parseInt(this.id);
+        if(IsPlayerTurn() == 1) {
+        board[index] = 'X';
+        $(this).off('click');
+        Print(board);
+      } else {
+        board[index] = 'O';
+        $(this).off('click');
+        Print(board);
+      }
+  });
+
+  function checkBoard(board){
+     for(var i=0; i<board.length; i++){
+         if(board[i] === "")
+            return false;
+     }
+
+     return true;
+  }
+
+
+
+///end
+});
+
+
+/*function Log(val) {
     console.log(val);
 }
-
+*/
 /*function IsEmpty(index){
     if(board[index] != '')
         return
 }
-
 function MakeStep() {
-
 }*/
 
-$(function () {
+
 
     /*while(true){
         MakeStep();
         var status = getCurrentStatus();
         if(status=== 'draw'){
-
             break;
-
         }
     }*/
 
 
 
-    $('.inbox').click(function () {
+  /*  $('.inbox').click(function () {
         var index = parseInt(this.id);
         board[index] = 'X';
         Print(board);
         // computer logic
-    });
-});
+    });*/

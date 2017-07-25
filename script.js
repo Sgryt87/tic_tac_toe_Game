ba900ec2e7261baaa20e3e158d30ac64ef33c9c4
Sergii <sergiigrytsaienko@gmail.com>
2017-07-25 04:12:04 +0000

    another update

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function IsPlayerTurn () {
 }
 
 // X or O
-
+ 
 $('.inbox').click(function () {
         var index = parseInt(this.id);
         if(IsPlayerTurn() == 1) {
@@ -31,14 +31,58 @@ $('.inbox').click(function () {
       }
   });
 
-  function checkBoard(board){
-     for(var i=0; i<board.length; i++){
-         if(board[i] === "")
-            return false;
-     }
+// if array is empty 
+function isEmpty () {
+   if(board.length < 1) {
+    return true;
+    } else {
+    return false;
+    }
+}
 
-     return true;
-  }
+
+
+//f whoWon() who won or draw
+function whoWon() {
+    var playerX;
+    var playerO;
+    var draw;
+     if ((board[0] === 'X')&&(board[1] === 'X')&&(board[2] === 'X')) {
+        return playerX;
+      } else if ((board[3] === 'X')&&(board[4] === 'X')&&(board[5] === 'X')) {
+        return playerX;
+      } else if ((board[6] === 'X')&&(board[7] === 'X')&&(board[8] === 'X')) {
+        return playerX;
+      } else if ((board[0] === 'X')&&(board[3] === 'X')&&(board[6] === 'X')) {
+        return playerX;
+      } else if ((board[1] === 'X')&&(board[4] === 'X')&&(board[7] === 'X')) {
+        return playerX;
+      } else if ((board[2] === 'X')&&(board[5] === 'X')&&(board[8] === 'X')) {
+        return playerX;
+      } else if ((board[0] === 'X')&&(board[4] === 'X')&&(board[8] === 'X')) {
+        return playerX;
+      } else if ((board[2] === 'X')&&(board[4] === 'X')&&(board[6] === 'X')) {
+        return playerX;
+      } else if ((board[0] === 'O')&&(board[1] === 'O')&&(board[2] === 'O')) {
+        return playerO;
+      } else if ((board[3] === 'O')&&(board[4] === 'O')&&(board[5] === 'O')) {
+        return playerO;
+      } else if ((board[6] === 'O')&&(board[7] === 'O')&&(board[8] === 'O')) {
+        return playerO;
+      } else if ((board[0] === 'O')&&(board[3] === 'O')&&(board[6] === 'O')) {
+        return playerO;
+      } else if ((board[1] === 'O')&&(board[4] === 'O')&&(board[7] === 'O')) {
+        return playerO;
+      } else if ((board[2] === 'O')&&(board[5] === 'O')&&(board[8] === 'O')) {
+        return playerO;
+      } else if ((board[0] === 'O')&&(board[4] === 'O')&&(board[8] === 'O')) {
+        return playerO;
+      } else if ((board[2] === 'O')&&(board[4] === 'O')&&(board[6] === 'O')) {
+        return playerO;
+      } else {
+        return draw;
+      }
+}
 
 
 

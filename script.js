2cc262eedef32e74ccc781f3384d4e46ad278d98
Sergii <sergiigrytsaienko@gmail.com>
2017-07-31 01:52:27 +0000

    updated

--- script.js
+++ script.js
@@ -1,121 +1,142 @@
-$(document).ready(function() {
-
-  var board = [
-      '', '', '',
-      '', '', '',
-      '', '', ''
+$(document).ready(function () {
+    // Game board
+    var board = [
+        '', '', '',
+        '', '', '',
+        '', '', ''
     ];
 
-  function Print(board) {
-      for (var i = 0; i < 9; i++) {
-          $('#' + i).text(board[i]);
-      }
-  }
-// user's turn
-function IsPlayerTurn () {
-  return Math.floor(Math.random()*2+1);
-}
+    // First and second player ('computer', 'user')
+    // First player plays with 'X'
+    var firstPlayer, secondPlayer;
 
-// X or O
- 
-$('.inbox').click(function () {
-        var index = parseInt(this.id);
-        if(IsPlayerTurn() == 1) {
-        board[index] = 'X';
-        $(this).off('click');
-        Print(board);
-      } else {
-        board[index] = 'O';
-        $(this).off('click');
-        Print(board);
-      }
-  });
-
-// if array is empty 
-function isEmpty () {
-   if(board.length < 1) {
-    return true;
-    } else {
-    return false;
-    }
-}
-
-
-
-//f whoWon() who won or draw
-function whoWon() {
-    var playerX;
-    var playerO;
-    var draw;
-     if ((board[0] === 'X')&&(board[1] === 'X')&&(board[2] === 'X')) {
-        return playerX;
-      } else if ((board[3] === 'X')&&(board[4] === 'X')&&(board[5] === 'X')) {
-        return playerX;
-      } else if ((board[6] === 'X')&&(board[7] === 'X')&&(board[8] === 'X')) {
-        return playerX;
-      } else if ((board[0] === 'X')&&(board[3] === 'X')&&(board[6] === 'X')) {
-        return playerX;
-      } else if ((board[1] === 'X')&&(board[4] === 'X')&&(board[7] === 'X')) {
-        return playerX;
-      } else if ((board[2] === 'X')&&(board[5] === 'X')&&(board[8] === 'X')) {
-        return playerX;
-      } else if ((board[0] === 'X')&&(board[4] === 'X')&&(board[8] === 'X')) {
-        return playerX;
-      } else if ((board[2] === 'X')&&(board[4] === 'X')&&(board[6] === 'X')) {
-        return playerX;
-      } else if ((board[0] === 'O')&&(board[1] === 'O')&&(board[2] === 'O')) {
-        return playerO;
-      } else if ((board[3] === 'O')&&(board[4] === 'O')&&(board[5] === 'O')) {
-        return playerO;
-      } else if ((board[6] === 'O')&&(board[7] === 'O')&&(board[8] === 'O')) {
-        return playerO;
-      } else if ((board[0] === 'O')&&(board[3] === 'O')&&(board[6] === 'O')) {
-        return playerO;
-      } else if ((board[1] === 'O')&&(board[4] === 'O')&&(board[7] === 'O')) {
-        return playerO;
-      } else if ((board[2] === 'O')&&(board[5] === 'O')&&(board[8] === 'O')) {
-        return playerO;
-      } else if ((board[0] === 'O')&&(board[4] === 'O')&&(board[8] === 'O')) {
-        return playerO;
-      } else if ((board[2] === 'O')&&(board[4] === 'O')&&(board[6] === 'O')) {
-        return playerO;
-      } else {
-        return draw;
-      }
-}
-
-
-
-///end
-});
-
-
-/*function Log(val) {
-    console.log(val);
-}
-*/
-/*function IsEmpty(index){
-    if(board[index] != '')
-        return
-}
-function MakeStep() {
-}*/
-
-
-
-    /*while(true){
-        MakeStep();
-        var status = getCurrentStatus();
-        if(status=== 'draw'){
-            break;
+    // Prepares all stuff
+    function Init() {
+        if (IsPlayerTurn() === 1){
+            firstPlayer = 'user';
+            secondPlayer = 'computer';
         }
-    }*/
+        else {
+            firstPlayer = 'computer';
+            secondPlayer = 'user';
+        }
+    }
 
+    // Print current game board to html page
+    function Print() {
+        for (var i = 0; i < 9; i++) {
+            $('#' + i).text(board[i]);
+        }
+    }
 
+    function EndGame(msg){
+        $('h1').text(msg);
+        $('.inbox').off('click');
+    }
 
-  /*  $('.inbox').click(function () {
+    // user's turn
+    function IsPlayerTurn() {
+        return Math.floor(Math.random() * 2 + 1);
+    }
+
+    // Returns true if three cells of the board contains same value
+    function IsSame(sign, cell1, cell2, cell3) {
+        return  (cell1 === sign) && (cell2 === sign) && (cell3 === sign);
+    }
+
+    // Returns true if there is a line on the board with specified sign
+    function IsLine(sign) {
+        return  IsSame(sign, board[0], board[1], board[2]) ||
+                IsSame(sign, board[3], board[4], board[5]) ||
+                IsSame(sign, board[6], board[7], board[8]) ||
+                IsSame(sign, board[0], board[3], board[6]) ||
+                IsSame(sign, board[1], board[4], board[7]) ||
+                IsSame(sign, board[2], board[5], board[8]) ||
+                IsSame(sign, board[0], board[4], board[8]) ||
+                IsSame(sign, board[6], board[4], board[2]);
+    }
+
+    // Returns true if all cells of the board are occupied
+    function IsFull() {
+        return  (board[0] != '') && (board[1] != '') && (board[2] != '') &&
+                (board[3] != '') && (board[4] != '') && (board[5] != '') &&
+                (board[6] != '') && (board[7] != '') && (board[8] != '');
+    }
+
+    // Returns
+    // 'computer' if computer won
+    // 'user' if user won
+    // 'draw' if board is full but nobody won
+    // 'none' if game continues
+    function GetGameStatus() {
+        if (IsLine('X')){
+            return firstPlayer;
+        } else if (IsLine('O')) {
+            return secondPlayer;
+        } else if (IsFull()){
+            return 'draw';
+        }
+        return 'none';
+    }
+
+    // OnClick event handler
+    $('.inbox').click(function () {
         var index = parseInt(this.id);
-        board[index] = 'X';
-        Print(board);
-        // computer logic
-    });*/
+        $(this).off('click');
+        if(firstPlayer === 'user'){
+            board[index] = 'X';
+        } else{
+            board[index] = 'O';
+        }
+        Print();
+        switch(GetGameStatus()){
+            case 'computer':
+                EndGame('You lose');
+                return;
+            case 'user':
+                EndGame('You won! Congratulations');
+                return;
+            case 'draw':
+                EndGame('Draw');
+                return;
+        }
+        ComputerMove();
+        Print();
+        switch(GetGameStatus()){
+            case 'computer':
+                EndGame('You lose');
+                return;
+            case 'user':
+                EndGame('You won! Congratulations');
+                return;
+            case 'draw':
+                EndGame('Draw');
+                return;
+        }
+    });
+
+    // Returns a random umber between [min, max]
+    function Rand(min, max){
+        return Math.floor(Math.random() * max) + min;
+    }
+
+    // Computer makes a move (AI)
+    function ComputerMove() {
+        var empty = [];
+        for(var i = 0; i < board.length; i++){
+            if (board[i] === ''){
+                empty.push(i);
+            }
+        }
+        var ind = Rand(0, empty.length - 1);
+        var sign = firstPlayer === 'computer' ? 'X' : 'O';
+        board[empty[ind]] = sign;
+    }
+
+    // Game loop
+    Init();
+    if (firstPlayer === 'computer'){
+        ComputerMove();
+        Print();
+    }
+});
